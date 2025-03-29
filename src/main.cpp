45192264c1b7253d7b9113ee1f1c36ad19b75251
Sebastian Rodriguez <331558+sebasrp@users.noreply.github.com>
2025-03-29 19:30:42 +0800

    fix the wokwi diagram for simulation

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,23 @@
 // put function declarations here:
 int myFunction(int, int);
 
-void setup() {
+void setup()
+{
+  Serial.begin(9600);
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  Serial.println("ping");
+  delay(2000);
+  Serial.println("pong");
+  delay(2000);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(int x, int y)
+{
   return x + y;
 }
